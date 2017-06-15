660ae55d50c1fdb64f890b098a885d76949678b6
Georg Sawtschuk <georg.the.moon@googlemail.com>
2017-06-15 03:28:54 +0200

    Added url parameters CSS

--- src/elements/Parameter.js
+++ src/elements/Parameter.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import SteemApi from "../steemjs/api";
 import types from "../steemjs/api/types.json";
+import ParameterCss from "./Parameter.css";
 
 class Parameter extends Component {
     
@@ -12,23 +13,32 @@ class Parameter extends Component {
   }  
     
   render() {
+    let paramValues = this.props.paramValues;
+    function getValue(name) {
+        if(paramValues[name] && paramValues[name] != "") {
+            return paramValues[name];
+        }
+        return null;
+    }
     
     let param = this.props.param;
-    
-    let input = <input type="text" name={param.name} onChange={this.props.onChange} />
+
+    let input = <input type="text" name={param.name} onChange={this.props.onChange} value = {getValue(param.name)}/>
     console.log(param.type);
     if(param.type == "Array") {
-        input = [0,1,2,3,4].map((pIdx) =>
-            <div ><input type="text" name={param.name + "_" + pIdx} onChange={this.props.onChange} /></div>
+        input = [0,1,2].map((pIdx) =>
+            <div><input type="text" name={param.name + "_" + pIdx} onChange={this.props.onChange} value = {getValue(param.name + "_" + pIdx)}/></div>
         );
     }    
     return (
-    <div>
-        <label title={param.desc.ru}>
+    <tr>
+        <td title={param.desc.ru} className="Parameter-name">
         {param.name}:&nbsp;
+        </td>
+        <td>
             {input}
-        </label>
-    </div>
+        </td>
+    </tr>
     );
   }
 }
--- src/pages/Api.js
+++ src/pages/Api.js
@@ -2,25 +2,28 @@ import React, { Component } from 'react';
 import SteemApi from "../steemjs/api";
 import Apis from "../elements/Apis";
 import Header from "../elements/Header";
+import ApiCss from "./Api.css";
 
 class Api extends Component {
     
   constructor() {
       super();
       this.steemapi = new SteemApi();
-      this.steemapi.dump();
+      //this.steemapi.dump();
   }  
     
   render() {
-    
+    document.title = "Steem-JS API";
     return (
     <div className="Api-body">
         <Header />
-        <h2>Steem API</h2>
-        <div className="Api-list"><Apis apis = {this.steemapi.methods}/></div>
-        <div className="Api-help"></div>
+        <div className="Api-content">
+            <h2>Steem API</h2>
+            <div className="Api-list"><Apis apis = {this.steemapi.methods}/></div>
+            <div className="Api-help"></div>
+        </div>
     </div>
-    );
+    );  
   }
 }
 
