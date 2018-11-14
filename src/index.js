1846e7a659c09485281c1d7c8a5d93c9abfbb641
Georg Sawtschuk <georg.the.moon@googlemail.com>
2018-11-14 15:37:30 +0100

    add history

--- src/index.js
+++ src/index.js
@@ -6,10 +6,15 @@ import registerServiceWorker from './registerServiceWorker';
 import './index.css';
 import { Route, BrowserRouter, Switch } from 'react-router-dom'
 
+import createBrowserHistory from "history/createBrowserHistory"
+
 import "bootstrap/dist/css/bootstrap.css"
+//import "bootstrap/dist/css/cerulean.css"
+
+const history = createBrowserHistory({basename: 'viz'});
 
 ReactDOM.render(
-  <BrowserRouter basename={'viz'}>
+  <BrowserRouter history={history} basename={'viz'}>
   <Switch>
       <Route exact path="/" component={(props) => <Api {...props} />} />
       <Route exact path="/api" component={(props) => <Api {...props} />}/>
